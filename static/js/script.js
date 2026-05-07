fa69f11161cfd497269441553185946796ee5f0f
Luna Baca <luna.baca@vallegrande.edu.pe>
2026-05-07 17:31:42 -0500

    Descripción de los cambios

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const name = document.getElementById('name').value;
@@ -22,3 +23,29 @@ document.getElementById('prev').addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     images[currentIndex].classList.remove('hidden');
 });
+=======
+class Carousel {
+    constructor(elementId) {
+        this.images = document.querySelectorAll(`#${elementId} img`);
+        this.currentIndex = 0;
+        this.prevButton = document.getElementById('prev');
+        this.nextButton = document.getElementById('next');
+        this.init();
+    }
+    init() {
+        this.nextButton.addEventListener('click', () => this.next());
+        this.prevButton.addEventListener('click', () => this.prev());
+    }
+    next() {
+        this.images[this.currentIndex].classList.add('hidden');
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        this.images[this.currentIndex].classList.remove('hidden');
+    }
+    prev() {
+        this.images[this.currentIndex].classList.add('hidden');
+        this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        this.images[this.currentIndex].classList.remove('hidden');
+    }
+}
+new Carousel('carousel');
+>>>>>>> Stashed changes
